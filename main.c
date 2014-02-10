f42dfeb5aed13f483e06afed6afb0cb95583e903
U-nico-PC\nico <nico@nico-PC.(none)>
2014-02-10 12:52:26 -0600

    Added thread_add_runqueue. Still working on thread_yield.

--- main.c
+++ main.c
@@ -7,23 +7,23 @@
 void f3(void *arg);
 void f2(void *arg);
 void f1(void *arg);
-struct thread *current;
+struct thread *current = NULL;
 
 typedef struct thread{
 	void (*f)(void *arg);								//function pointer
 	void* arg;											//function's argument
 	char* stack;										//thread's stack
-	//thread pointer maybe??
+	struct thread* nextThread;									//pointer to next thread in queue
 	char* base;											//stack base pointer
 } thread;
 
+//creates a new thread and initializes values
 thread *thread_create(void (*f)(void *arg), void *arg){
 	thread* newThread = malloc(sizeof(thread));			//new thread	
 	newThread->stack = malloc(stack_size);				//new stack
 	newThread->base = newThread->stack;					//set base pointer
 	printf("address = %p\n", (void*)newThread->stack);
 	printf("mod = %d\n", (int)newThread->stack % 8);
-	//newThread->stack++;
 	while((long)newThread->stack % 8 != 0){				//correct?
 		newThread->stack++;
 		printf("address = %p\n", (void*)newThread->stack);
@@ -38,26 +38,56 @@ thread *thread_create(void (*f)(void *arg), void *arg){
 	  printf("address = %p\n", (void*)newThread->stack);
 	  printf("mod = %d", (int)newThread->stack % 8);
 	}
+	printf("newThread address = %p\n", newThread);
 	return newThread;
+}
+
+//adds a new thread to the runqueue
+void thread_add_runqueue(thread* newThread){
+	printf("current thread = %p\nadding to queue...\n", current);
 	
+	//if empty queue
+	if(current == NULL){								//current thread = newThread, otherwise...
+		current = newThread;
+		current->nextThread = newThread;				//point to itself if only one in queue
+		printf("current's next thread = %p\n", current->nextThread);
+	}
+	//else, not empty queue
+	else{
+		newThread->nextThread = current->nextThread;	//point newThread to current's next thread
+		current->nextThread = newThread;				//current's next thread is the new thread
+		//this always puts the new thread right after the current thread
+	}
+	printf("current thread = %p\n", current);
+	return;
+}
+
+//saves the current thread's context to its struct
+void thread_yield(void){
+	//save cpu's base pointer and stack pointer to current thread
+	//move next thread's base pointer and stack pointer to cpu
+	printf("current thread = %p\n", current);
+	//save context
+	__asm __volatile("mov %%rsp, %%rax" : "=a" (current->stack) : );
+	__asm __volatile("mov %%rbp, %%rax" : "=a" (current->base) : );
+	//move new context in
+	__asm __volatile("mov %%rax, %%rsp" : : "a" (current->nextThread->stack) );
+	__asm __volatile("mov %%rax, %%rbp" : : "a" (current->nextThread->stack) );
+	
+	current = current->nextThread;
+	printf("current thread = %p\n", current);
+	return;
 }
 
 int main(int argc, char **argv)
 {
     struct thread *t1 = thread_create(f1, NULL);
-    /* thread_add_runqueue(t1);
+    thread_add_runqueue(t1);
+	/*
     thread_start_threading();
     printf("\nexited\n"); */
     return 0;
 }
-
-
-
-void thread_yield(void){
-
-}
-
-
 
 void f3(void *arg)
 {
