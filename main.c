d8a5116d5d3f4c6953b30b7e538efe8906c914be
Jared McArthur <jaredmc@cs.utexas.edu>
2014-02-15 17:05:48 -0600

    converted char* to uint64_t*, began standardizing debug output, renamed lastThread to prevThread

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <setjmp.h>
+#include <stdint.h>
 #include "threads.h"
 
 //#define MAX_THREADS 10
 #define stack_size 4096
-#define debug 0
+#define debug 1
 
 //static jmp_buf buffers[MAX_THREADS];			//holds jum_buf's for use with setjmp and longjmp.
 static jmp_buf mainBuf;							//buffer for main thread execution
@@ -21,10 +22,10 @@ typedef struct thread{
 	jmp_buf buffer;							//holds context info for switching
 	void (*f)(void *arg);					//function pointer
 	void* arg;								//function's argument
-	char* stack;							//thread's stack
+	uint64_t* stack;							//thread's stack
 	struct thread* nextThread;				//pointer to next thread in queue
-	struct thread* lastThread;				//pointer to previous thread
-	char* base;								//stack base pointer
+	struct thread* prevThread;				//pointer to previous thread
+	uint64_t* base;								//stack base pointer
 } thread;
 
 //creates a new thread and initializes values
@@ -34,52 +35,56 @@ thread *thread_create(void (*f)(void *arg), void *arg, int id){			//remove id be
 	newThread->firstRun = 0;					//0 for hasnt run yet
 	newThread->stack = malloc(stack_size);					//new stack
 	newThread->base = newThread->stack;					//set base pointer
-	printf("address = %p\n", (void*)newThread->stack);
-	printf("mod = %d\n", (int)newThread->stack % 8);
-	while((long)newThread->stack % 8 != 0){					//correct?
+	//printf("address = %p\n", (void*)newThread->stack);
+	//printf("mod = %d\n", (int)newThread->stack % 8);
+
+	while((uint64_t)newThread->stack % 8 != 0){					//correct?
 		newThread->stack++;
-		printf("address = %p\n", (void*)newThread->stack);
+		if (debug) {
+			printf("\naddress = %p\n", newThread->stack);
+		}
 	}
+
 	if (debug){
-	  printf("f = %p\n", f);
+	  printf("<in thread_create>\nf = %p\n", f);
 	}
 	newThread->f = f;							//function pointer
 	printf("newThread->f = %p\n", newThread->f);
 	newThread->arg = arg;							//void* arg
 	if (debug) {
-	  printf("address = %p\n", (void*)newThread->stack);
-	  printf("mod = %d", (int)newThread->stack % 8);
+	  printf("address = %p\n", newThread->stack);
+	  printf("mod = %lu\n", (uint64_t)newThread->stack % 8);
 	}
-	printf("newThread address = %p\n", newThread);
+	printf("newThread address = %p\n<leaving thread_create>\n", newThread);
 	return newThread;
 }
 
 //adds a new thread to the runqueue
 void thread_add_runqueue(thread* newThread){
-	printf("current thread = %p\nadding to queue...\n", current);
+	printf("\n<in thread_add_runqueue>\ncurrent thread = %p\nadding to queue...\n", current);
 	
 	//if empty queue
 	if(current == NULL){							//current thread = newThread, otherwise...
 		current = newThread;
 		current->nextThread = newThread;				//point to itself if only one in queue
-		current->lastThread = newThread;				//^^
+		current->prevThread = newThread;				//^^
 		printf("current's next thread = %p\n", current->nextThread);
 	}
 	//else, not empty queue
 	else{
-		current->lastThread->nextThread = newThread;			//last thread needs to point to a new last thread
-		newThread->lastThread = current->lastThread;	//new thread's previous thread points to the previous last thread
+		current->prevThread->nextThread = newThread;			//last thread needs to point to a new last thread
+		newThread->prevThread = current->prevThread;	//new thread's previous thread points to the previous last thread
 		newThread->nextThread = current;							
-		current->lastThread = newThread;
+		current->prevThread = newThread;
 	}
-	printf("current thread = %p\n\n", current);
+	printf("current thread = %p\n<leaving thread_add_runqueue>\n", current);
 	return;
 }
 
 //saves the current thread's context to its struct
 void thread_yield(void){
 	if(debug){
-		printf("current thread = %p\n", current);
+		printf("\n<in thread_yield>\ncurrent thread = %p\n", current);
 		printf("current's stack pointer = %p\n", current->stack);
 		printf("current's base pointer = %p\n", current->base);
 	}		
@@ -95,9 +100,9 @@ void thread_yield(void){
 	
 	
 	if(debug){
-		printf("current's stack pointer = %p\n", current->stack);
+		printf("\ncurrent's stack pointer = %p\n", current->stack);
 		printf("current's base pointer = %p\n", current->base);
-		printf("current thread = %p\n", current);
+		printf("current thread = %p\n<leaving thread_yield>\n", current);
 	}
 	return;
 }
@@ -135,13 +140,13 @@ void thread_start_threading(void){
 
 void thread_exit(void){
 	if(debug){
-		printf("current thread = %p\n", current);
-		printf("last thread id = %d\n", current->lastThread->id);
-		printf("next thread id = %d\n", current->nextThread->id);
+		printf("\n<in thread_exit>\ncurrent thread = %p\n", current);
+		printf("last thread id = %d\n", current->prevThread->id);
+		printf("next thread id = %d\n<leaving thread_exit>\n", current->nextThread->id);
 	}
 	thread* oldThread = current;
-	current->lastThread->nextThread = current->nextThread;			//removing from ring
-	current->nextThread->lastThread = current->lastThread;
+	current->prevThread->nextThread = current->nextThread;			//removing from ring
+	current->nextThread->prevThread = current->prevThread;
 	current = current->nextThread;						//move to next thread
 	free(oldThread->stack);										
 	free(oldThread);		
@@ -149,25 +154,34 @@ void thread_exit(void){
 }
 
 //prints the ring of threads
-void printRing(int revs, int numThreads){
+	void printRing(int revs, int numThreads){			//revs=times around the ring
 	int i = numThreads * revs;
+	if (debug) { printf("\n<in printRing>\n"); }
 	while(i > 0){
 		printf("thread id = %d\n", current->id);
 		current = current->nextThread;
 		i--;
 	}
+	if (debug) { printf("\n<leaving printRing>\n"); }
 }
 
 int main(int argc, char **argv)
 {
 	int i = 0;
-    struct thread *t1 = thread_create(f1, NULL, 1);
+
+        thread *t1 = thread_create(f1, NULL, 1);
+//	thread *t2 = thread_create(f1, NULL, 2);
+/*
 	thread* t2 = thread_create(f1, NULL, 2);
 	thread* t3 = thread_create(f1, NULL, 3);
 	thread* t4 = thread_create(f1, NULL, 4);
 	thread* t5 = thread_create(f1, NULL, 5);
-    thread_add_runqueue(t1);
+*/    
+	thread_add_runqueue(t1);
 	i++;
+//	thread_add_runqueue(t2);
+//        i++;
+/*
 	thread_add_runqueue(t2);
 	i++;
 	thread_add_runqueue(t3);
@@ -176,20 +190,25 @@ int main(int argc, char **argv)
 	i++;
 	thread_add_runqueue(t5);
 	i++;
-	printRing(3, i);
+*/
+	printRing(1, i);
+
 	thread_exit();
 	i--;
+/*
 	thread_exit();
 	i--;
-	t1 = thread_create(f1, NULL, 1);
-	thread_add_runqueue(t1);
-	i++;
-	printRing(3, i);
+
+	printRing(1, i);
 	
+	thread_exit();
+	i--;
 	
-	/*
+	printRing(1, i);
+	
     thread_start_threading();
-    printf("\nexited\n"); */
+    printf("\nexited\n"); 
+*/
     return 0;
 }
 
@@ -229,4 +248,5 @@ void f1(void *arg)
         }
         thread_yield();
     } */
+    printf("Hello\n");
 } 
